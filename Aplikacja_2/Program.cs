745828fbb61cdfc492430f3c9b23acd45786cf82
aaa <bajo@jajo.com>
2023-04-27 17:04:55 +0200

    c2

--- Aplikacja_2/Program.cs
+++ Aplikacja_2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// connect
+using var client = new HttpClient();
+client.DefaultRequestHeaders.Add("User-Agent", "C# console program");
+
+// get content
+var content = await client.GetStringAsync("https://konni.delthas.fr/games");
+
+Console.WriteLine(content);
+
+Console.ReadLine();
+
+// fetch list
+content = await client.GetStringAsync("https://konni.delthas.fr/games");
+Console.WriteLine(content);
+//
